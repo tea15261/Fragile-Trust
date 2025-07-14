cd25c9427e7972b60dadf212d932b6bc683eca25
Atiksh Jha <atikshjha2007@gmail.com>
2025-07-14 11:48:04 -0400

    chnaged order

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ const config = {
             debug: false // collision debugging
         }
     },
-    scene: [TavernScene, ForestScene, BattleScene], 
+    scene: [BattleScene, TavernScene, ForestScene], 
     scale: {
         mode: Phaser.Scale.FIT, 
         autoCenter: Phaser.Scale.CENTER_BOTH 
